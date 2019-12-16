1c2cab04c750b604b4be0906fa6a98643499324b
Clarke Cirel <Clarke.cirel@gmail.com>
2019-12-16 16:16:13 -0700

    add components and app.js update

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import Register from './components/Register';
+import { Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import FetchUser from './components/FetchUser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <NavBar/>
+    <Container>
+      <FetchUser>
+        <Switch>
+          <Route exact path = '/' component = {Home} />
+          <Route exact path = '/Login' component = {Login} />
+          <Route exact path = '/Register' component = {Register} />
+          <Route component = {NoMatch} />
+        </Switch>
+      </FetchUser>
+    </Container>
+  </>
+);
 
 export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,1 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const Home = () => (
+<Header as="h2" textAlign="center">Zelp App</Header>
+)
+
+export default Home;
